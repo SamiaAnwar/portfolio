6dc328409f0ad18bf954c078fca57393495f2b3c
SamiaAnwar <samia.anwar123@gmail.com>
2025-02-28 17:05:17 -0500

    tried to implement global styles

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,17 @@
 const config = {
+  theme: {
+    extend: {
+        colors: {
+            lightHover: '#fdf4ff', 
+            darkHover: '#2a004a',
+            darkTheme: '#11001F'
+        },
+        fontFamily: {
+            Outfit: ['Outfit', 'sans-serif'],
+            Ovo: ['Ovo', 'serif']
+        }
+    },
+  },  
   plugins: ["@tailwindcss/postcss"],
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,11 @@ export default {
             colors: {
                 lightHover: '#fdf4ff', 
                 darkHover: '#2a004a',
+                darkTheme: '#11001F'
+            },
+            fontFamily: {
+                Outfit: ['Outfit', 'sans-serif'],
+                Ovo: ['Ovo', 'serif']
             }
         },
     },
