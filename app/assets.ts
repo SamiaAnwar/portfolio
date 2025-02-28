73ce8ced5df6669d8fa41719e27ef94c00884622
SamiaAnwar <samia.anwar123@gmail.com>
2025-02-28 17:05:51 -0500

    added nav bar graphics

--- app/assets.ts
+++ app/assets.ts
@@ -1,3 +1,15 @@
 export const logo = {
     src: "/sunflower.png"
 };
+
+export const darkModeIcon = {
+    src: "/moon.svg"
+};
+
+export const menuIcon = {
+    src: "/menu.svg"
+};
+
+export const close = {
+    src: "/close.svg"
+};
