6315edb417bf78afafb70eb2de1f729bbea814ed
SamiaAnwar <samia.anwar123@gmail.com>
2025-03-02 14:38:21 -0500

    added an about component

---
+++ app/components/About.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const About = () => {
+    return (
+        <div id="about" className="w-full px-[12%] py-10 scroll-mt-20">
+            <h4 className="text-center mb-2 text-lg fancy"> Introduction </h4>
+            <h2 className=" text-center text-5xl fancy"> About me </h2>
+        </div>
+    ); 
+}; 
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 import Image from "next/image";
 import { NavBar } from "./components/NavBar";
 import { Header } from "./components/Header"; 
+import { About } from "./components/About";
 
 export default function Home() {
   return (
     <>
       <NavBar />
       <Header />
+      <About />
     </>
   );
 }
