861ffcfe9cbf0de70d8fd528279fe8e0d1a650a5
SamiaAnwar <samia.anwar123@gmail.com>
2025-03-02 14:11:47 -0500

    display header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 import Image from "next/image";
 import { NavBar } from "./components/NavBar";
+import { Header } from "./components/Header"; 
 
 export default function Home() {
   return (
     <>
-      <NavBar/>
+      <NavBar />
+      <Header />
     </>
   );
 }
