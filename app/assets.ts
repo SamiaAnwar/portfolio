4770a8c7ca4aa82a0c386175e0404ca9ec0ee901
SamiaAnwar <samia.anwar123@gmail.com>
2025-03-03 15:08:00 -0500

    added logos

--- app/assets.ts
+++ app/assets.ts
@@ -26,4 +26,25 @@ export const resume = {
 export const logos = {
     github: "/github_logo.svg",
     linkedin: "/linkedin_logo.svg"
-}
+};
+
+export const about = {
+    grad: "/GradShot.jpeg"
+};
+
+export const techStack = {
+    aws: '/aws.svg',
+    docker: '/docker.svg',
+    figma: '/figma.svg',
+    flask: '/flask.svg',
+    gcr: '/google-cloud-run.svg',
+    js: '/javascript.svg',
+    nextjs: '/nextjs.svg',
+    psql: '/postgresql.svg',
+    python: '/python.svg',
+    react: '/react.svg',
+    supabase: '/supabase.svg',
+    twcss: '/tailwindcss.svg',
+    ts: '/typescript.svg',
+    vue: '/vue.svg'
+}; 
