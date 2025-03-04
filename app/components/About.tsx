ad032fce60f2b581361841af20aa2cac8eff684f
SamiaAnwar <samia.anwar123@gmail.com>
2025-03-04 10:57:34 -0500

    updated layout of tech stack icons

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -5,13 +5,12 @@ import next from "next";
 
 export const About = () => {
     return (
-        <div id="about" className=" w-full px-[12%] py-10 scroll-mt-20">
+        <div id="about" className=" relative w-full px-[12%] py-10">
             <h4 className="text-center mb-2 text-lg fancy"> Introduction </h4>
             <h2 className=" text-center text-5xl fancy"> About Me </h2>
-            
-            <div className="flex w-full flex-col lg:flex-row items-start gap-20 my-20">
+            <div className="flex w-full flex-col lg:flex-row items-center gap-10 my-20">
                 <div className="flex justify-center sm:flex-row h-100% w-100% items-center">
-                    <Image src={about.grad} alt="headshot" width={300} height={450} className="rounded-lg w-32 sm:w-48 md:w-64 lg:w-80 xl:w-96 h-auto"/>
+                    <Image src={about.grad} alt="headshot" width={300} height={450} className="rounded-lg w-50 sm:w-70 lg:w-70 xl:w-80 h-auto"/>
                 </div>
                 <div className="flex-1 text-lg">
                     <ul className="mb-10 max-w-2xl">
@@ -20,20 +19,33 @@ export const About = () => {
                         <li className="fancy"> &#128187; Currently, I'm honing my skills in full stack development, keep scrolling to learn about the projects I've worked on!  </li>
                         <li className="fancy"> &#128640; I'm looking for full-time roles where I can apply my skills to create powerful software products. </li>
                     </ul>
-                    <h2 className="text-left mb-2 text-lg fancy "> my tech stack (so far...) </h2>
-                    <ul>
-                        <li>
-                            <Image src={techStack.python} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3" /> <Image src={techStack.js} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3" />
-                            <Image src={techStack.react} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3" /> 
-                            <Image src={techStack.ts} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3" /> 
-                            <Image src={techStack.twcss} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3"/> 
-                            <Image src={techStack.flask} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3" /> 
-                            <Image src={techStack.aws} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3" /> 
-                            <Image src={techStack.gcr} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3" /> 
-                            <Image src={techStack.supabase} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3" /> 
-                            <Image src={techStack.psql} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3"/>
-                        </li>
-                    </ul>
+                    <div className="relative w-full my-6 rounded-lg max-w-2x flex flex-col md:flex-row ">
+                        <div>
+                            <div className="mb-4 rounded-full bg-teal-600 py-0.5 px-1 border border-transparent text-xs text-white transition-all shadow-sm w-20 text-center ">LANGUAGES</div>
+                            <div className=" p-2">
+                                <Image src={techStack.python} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3" /> <Image src={techStack.js} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3" />
+                            </div>
+                        </div>
+                        <div >
+                            <div className="mb-4 rounded-full bg-pink-600 py-0.5 px-1 border border-transparent text-xs text-white transition-all shadow-sm w-20 text-center">FRONTEND</div>
+                            <div className=" p-2">
+                                <Image src={techStack.react} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3" />
+                                <Image src={techStack.ts} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3" />
+                                <Image src={techStack.twcss} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3"/>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="mb-4 rounded-full bg-purple-600 py-0.5 px-1 border border-transparent text-xs text-white transition-all shadow-sm w-20 text-center">BACKEND</div>
+                            <div className=" p-2">
+                                <Image src={techStack.flask} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3" />
+                                <Image src={techStack.aws} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3" />
+                                <Image src={techStack.gcr} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3" />
+                                <Image src={techStack.docker} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3"/>
+                                <Image src={techStack.supabase} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3" />
+                                <Image src={techStack.psql} alt="tech" width={300} height={450} className="rounded-lg w-10 h-auto inline-block m-3"/>
+                            </div>
+                        </div>
+                    </div> 
                 </div>
             </div>
         </div>
