d09841a0172e4e96acdabaa5c285b13a5d29a8b5
SamiaAnwar <samia.anwar123@gmail.com>
2025-03-05 10:55:05 -0500

    added information for future components

--- app/assets.ts
+++ app/assets.ts
@@ -48,3 +48,50 @@ export const techStack = {
     ts: '/typescript.svg',
     vue: '/vue.svg'
 }; 
+
+export const projects = [
+    {
+        name: 'My Health Pal',
+        desc: 'As a part of my capstone project, I created an end-to-end web application to help families track their healthcare appointments with live user authentication.',
+        src: '/myhealthpal.png',
+        link: 'https://docs.google.com/presentation/d/1zJ5XNRF-AXiS6I4aMblWQ49aDNDh00HG/edit?usp=sharing&ouid=111583658904153071317&rtpof=true&sd=true',
+        tags: ['GraphQL', 'DynamoDB', 'Systems Design', 'React.js', 'Node.js']
+    },
+    {
+        name: 'Trading Simulator',
+        desc: 'To exercise my coding skills, I trained a Random Forest Regressor to make 30-day predictions from daily stock market closing and simulate trades. It is deployed as a live site to visualise current performance of the decisions model.',
+        src: '/tradesim',
+        link: 'https://samiaanwar.github.io/trading-simulator/',
+        tags: ['Machine Learning', 'scikit-learn', 'Flask', 'React.js', 'TailwindCSS', 'Google Cloud Run', 'Docker']
+    },
+    {
+        name: 'matisse',
+        desc: '',
+        src: '/matisse',
+        link: '',
+        tags: ['npm', 'JavaScript', 'JUnit'],
+    }
+]
+
+export const experiences = [
+    {
+        name: "Healthcare Systems R&A",
+        date: 'May 2022 - Aug 2022',
+        desc: "",
+        src: "",
+        tags: ['TensorFlow','OpenCV', 'pandas', 'Data Cleaning', 'Literature Review']
+    },
+    {
+        name: "Llettonna Foundation",
+        date: 'Feb 2020 - Jul 2021',
+        desc: "",
+        src: "",
+        tags: ['Machine Learning', 'Project Management', 'Data Cleaning', 'Publication']
+    },
+    {
+        name: "Fyelabs",
+        desc: "Apr 2020 - Aug 2020",
+        src: "",
+        tags: ['Vue.js','Flutter', 'DevOps', 'Consulting']
+    }
+]
