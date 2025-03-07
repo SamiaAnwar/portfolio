1967698090b9b5ae2ce1c11169559f7a15822505
SamiaAnwar <samia.anwar123@gmail.com>
2025-03-07 11:04:38 -0500

    made the contact section compatible with darkmode

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,8 +1,10 @@
 import React, { useState, FormEvent } from "react";
 import { Send } from 'lucide-react';
 
-
-export const Contact = () => {
+type Props = {
+    isDarkMode: boolean; 
+}
+export const Contact = ({isDarkMode}: Props) => {
     const [result, setResult] = useState(""); 
     async function handleSubmit(e: FormEvent<HTMLFormElement>) {
         e.preventDefault(); 
@@ -40,12 +42,12 @@ export const Contact = () => {
             </p>
             <form onSubmit={ handleSubmit } className="max-w-2xl mx-auto">
                 <div className="grid grid-cols-2 gap-6 mt-10 mb-8">
-                    <input type="text" placeholder="Enter your name" required className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white" name="name"/> 
+                    <input type="text" placeholder="Enter your name" required className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white text-gray-900" name="name"/> 
                     <input type="email" placeholder="Enter your email" required className="flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white
-                    " name="email"/> 
+                    text-gray-900" name="email"/> 
                 </div>
-                <textarea rows={6} placeholder="Enter your message" required className="w-full p-4 outline-none border-[0.5px] border-gray-400 rouned-md bg-white mb-6" name="message"></textarea>
-                <button type="submit" className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500"> Send <Send className="w-5 h-5 text-white" /></button>
+                <textarea rows={6} placeholder="Enter your message" required className="w-full p-4 outline-none border-[0.5px] border-gray-400 rouned-md bg-white mb-6 text-gray-900" name="message"></textarea>
+                <button type="submit" className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 border border-white"> Send <Send className="w-5 h-5 text-white" /></button>
                 <p className="mt-4"> {result}</p>
             </form>
         </div>
