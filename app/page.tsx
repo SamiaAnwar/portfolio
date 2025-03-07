fb784e1c5ab72eaaef86db2817e1fe07087a039d
SamiaAnwar <samia.anwar123@gmail.com>
2025-03-07 11:04:06 -0500

    set up dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,44 @@ import { Projects } from "./components/Projects";
 import { Experiences } from "./components/Experiences";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
+import { useEffect, useState } from "react";
 export default function Home() {
+  const [isDarkMode, setIsDarkMode] = useState(false); 
+
+  useEffect(() => {
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      setIsDarkMode(true)
+    } else {
+      setIsDarkMode(false)
+    }
+  }, [])
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark');
+      localStorage.theme = 'dark';
+    } else {
+      document.documentElement.classList.remove('dark')
+      localStorage.theme = '';
+    }
+  }, [isDarkMode])
+
+  const handleSetDark = () => {
+    if (isDarkMode) {
+        setIsDarkMode(false)
+    } else {
+        setIsDarkMode(true)
+    }
+}
+
   return (
     <>
-      <NavBar />
-      <Header />
-      <About />
-      <TechStack />
-      <Projects /> 
-      <Experiences /> 
-      <Contact /> 
-      <Footer /> 
+      <NavBar isDarkMode={ isDarkMode } handleSetDark={ handleSetDark }/>
+      <Header isDarkMode={ isDarkMode }/>
+      <About/>
+      <TechStack isDarkMode={ isDarkMode }/>
+      <Projects isDarkMode={ isDarkMode }/> 
+      <Experiences isDarkMode={ isDarkMode }/> 
+      <Contact isDarkMode={ isDarkMode }/> 
     </>
   );
 }
