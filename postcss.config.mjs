fe207c01f1fe56cca9ab99bc60ededb4dec51342
SamiaAnwar <samia.anwar123@gmail.com>
2025-03-07 12:45:48 -0500

    removed theme

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,17 +1,4 @@
 const config = {
-  theme: {
-    extend: {
-        colors: {
-            lightHover: '#fdf4ff', 
-            darkHover: '#2a004a',
-            darkTheme: '#11001F'
-        },
-        fontFamily: {
-            Outfit: ['Outfit', 'sans-serif'],
-            Ovo: ['Ovo', 'serif']
-        }
-    },
-  },  
   plugins: ["@tailwindcss/postcss"],
 };
 
